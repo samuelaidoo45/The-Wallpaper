1f2b5e247a75f08f36e262cdec6bd129784fc3f0
4eug <debraheug@gmail.com>
2021-01-24 13:28:01 +0000

    Initial Commit (Few Changes)

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaperapp/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'The Wallpaper App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: Home(),
+    );
+  }
+}
